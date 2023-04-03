4d12019dd14b667a61bafeee9f71d676c1824f49
Arman-Kabir <armankabir8@gmail.com>
2023-04-03 19:22:43 +0600

    github issues

---
+++ pre-requisite.js
@@ -0,0 +1,6 @@
+
+/*
+    OpenGL : OpenGL is a link between the application and the graphics card driver more precisely. 
+
+    WebGl: WebGL is a link between the Js application and the graphics crad driver more precisely.
+*/
